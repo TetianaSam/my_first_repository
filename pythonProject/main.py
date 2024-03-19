7a4fccb41b5006b899654d50b6833cdc93543756
Tana <samoilenkotanya1989@gmail.com>
2024-03-19 19:54:40 +0100

    create program spine

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,1 +1,23 @@
-print("Hello world")
+
+
+
+
+def print_menu():
+    pass
+def search_item():
+    pass
+def add_item():
+    pass
+def print_items():
+    pass
+def del_item():
+    pass
+def replace_item():
+    pass
+def get_input():
+    pass
+
+
+def run():
+    pass
+run()
